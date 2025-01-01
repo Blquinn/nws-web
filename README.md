e32d4da87641a65e95c35c98ceb2923b8ed3f7db
Benjamin Quinn <benjamin.quinn92@gmail.com>
2025-01-01 10:29:12 -0500

    maplibre

--- README.md
+++ README.md
@@ -1,38 +1,14 @@
-# sv
+Download pmtiles cli https://docs.protomaps.com/pmtiles/cli
 
-Everything you need to build a Svelte project, powered by [`sv`](https://github.com/sveltejs/cli).
+Download partial pmtiles from https://maps.protomaps.com/builds/ with command:
 
-## Creating a project
-
-If you're seeing this, you've probably already done this step. Congrats!
-
-```bash
-# create a new project in the current directory
-npx sv create
-
-# create a new project in my-app
-npx sv create my-app
+```sh
+.\pmtiles.exe extract --maxzoom=6 https://build.protomaps.com/20241230.pmtiles ./tiles/20241230-z6.pmtiles
 ```
 
-## Developing
+Serve that directory with webserver that can handle range requests 
+like https://github.com/static-web-server/static-web-server
 
-Once you've created a project and installed dependencies with `npm install` (or `pnpm install` or `yarn`), start a development server:
-
-```bash
-npm run dev
-
-# or start the server and open the app in a new browser tab
-npm run dev -- --open
+```sh
+./static-web-server -p 8080 -d ./tiles -c *
 ```
-
-## Building
-
-To create a production version of your app:
-
-```bash
-npm run build
-```
-
-You can preview the production build with `npm run preview`.
-
-> To deploy your app, you may need to install an [adapter](https://svelte.dev/docs/kit/adapters) for your target environment.
--- src/routes/radar/+page.svelte
+++ src/routes/radar/+page.svelte
@@ -1,48 +1,50 @@
 <script lang="ts">
-	import * as L from 'leaflet';
 	import { onMount } from 'svelte';
+	import maplibregl, { type StyleSpecification } from 'maplibre-gl';
+	import { PMTiles, Protocol } from 'pmtiles';
 
-	import 'leaflet/dist/leaflet.css';
+	import 'maplibre-gl/dist/maplibre-gl.css';
+	import style from './pmtiles-style.json';
+
+	import nyc from './nyc.json';
 
 	let chartEl: HTMLDivElement;
-	let map: L.Map;
+	let map: maplibregl.Map;
 
 	onMount(() => {
-		map = L.map(chartEl, {
-			center: [40.6959883, -73.9953226],
-			zoom: 13
+		const pmTilesUrl = 'http://localhost:8080/20241230-z6.pmtiles';
+
+		const protocol = new Protocol();
+		const pmtiles = new PMTiles(pmTilesUrl);
+		protocol.add(pmtiles);
+		maplibregl.addProtocol('pmtiles', protocol.tilev4);
+
+		map = new maplibregl.Map({
+			container: chartEl,
+			center: [-73.9953226, 40.6959883],
+			zoom: 6,
+			style: style as StyleSpecification
 		});
 
-		const labelsPane = map.createPane('labels');
-		labelsPane.style.zIndex = '650';
-		labelsPane.style.pointerEvents = 'none';
+		map.on('load', () => {
+			map.addSource('nyc', {
+				type: 'geojson',
+				data: nyc as any
+			});
+			map.addLayer({
+				id: 'nyc',
+				type: 'fill',
+				source: 'nyc',
+				layout: {},
+				paint: {
+					'fill-color': '#088',
+					'fill-opacity': 0.8
+				}
+			});
+		});
 
-		L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-			attribution: '©OpenStreetMap, ©CartoDB'
-		}).addTo(map);
-
-		// L.tileLayer('https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png', {
-		// 	attribution: '©OpenStreetMap, ©CartoDB'
-		// }).addTo(map);
-		// L.tileLayer('https://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}.png', {
-		// 	attribution: '©OpenStreetMap, ©CartoDB',
-		// 	pane: 'labels'
-		// }).addTo(map);
-
-		L.control.scale().addTo(map);
-
-		// const geojson = L.geoJson(nyc as any, {}).addTo(map);
-
-		// geojson.eachLayer((layer) => {
-		// 	layer.bindPopup((layer as any).feature.properties.name);
-		// });
-
-		// map.fitBounds(geojson.getBounds());
-
-		return () => {
-			map.remove();
-		};
+		// return map.remove;
 	});
 </script>
 
-<div bind:this={chartEl} class="h-full"></div>
+<div bind:this={chartEl} class="h-full bg-white"></div>
